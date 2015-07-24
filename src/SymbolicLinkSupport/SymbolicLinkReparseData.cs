e5872edea1db724dff4523990d478129cdac4596
Michael Melancon <michaelmelancon@gmail.com>
2015-07-24 14:50:33 -0500

    Initial code commit.

---
+++ src/SymbolicLinkSupport/SymbolicLinkReparseData.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SymbolicLinkSupport
+{
+    /// <remarks>
+    /// Refer to http://msdn.microsoft.com/en-us/library/windows/hardware/ff552012%28v=vs.85%29.aspx
+    /// </remarks>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SymbolicLinkReparseData
+    {
+        // Not certain about this!
+        private const int maxUnicodePathLength = 260 * 2;
+
+        public uint ReparseTag;
+        public ushort ReparseDataLength;
+        public ushort Reserved;
+        public ushort SubstituteNameOffset;
+        public ushort SubstituteNameLength;
+        public ushort PrintNameOffset;
+        public ushort PrintNameLength;
+        public uint Flags;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = maxUnicodePathLength)]
+        public byte[] PathBuffer;
+    }
+}
